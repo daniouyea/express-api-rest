c16f976b21e8a71c4d062152079d11d41e667c16
Dani F. Serch <daniouyea@gmail.com>
2022-04-11 12:02:06 +0200

    Prisma schema: Post.content TEXT type

---
+++ prisma/migrations/20220411094804_init/migration.sql
@@ -0,0 +1,14 @@
+-- DropForeignKey
+ALTER TABLE `Post` DROP FOREIGN KEY `Post_authorId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `Profile` DROP FOREIGN KEY `Profile_userId_fkey`;
+
+-- AlterTable
+ALTER TABLE `Post` MODIFY `authorId` INTEGER NULL;
+
+-- AddForeignKey
+ALTER TABLE `Profile` ADD CONSTRAINT `Profile_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
